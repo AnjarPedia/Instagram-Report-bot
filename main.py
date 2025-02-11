a0934379700a131de4816d8bbcc90a7e1c5a3724
AnjarPedia <156521385+AnjarPedia@users.noreply.github.com>
2025-02-12 00:42:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ def keep_alive():
 keep_alive()
 
 # Initialize the Telegram bot
-API_TOKEN = os.getenv("API_TOKEN")
+API_TOKEN = os.getenv("7751886699:AAGSgDwDiv5x4esUb_fg8PopkA65eD8XGzw")
 FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL")
-ADMIN_ID = os.getenv("ADMIN_ID")
+ADMIN_ID = os.getenv("7688853513")
 
 bot = telebot.TeleBot(API_TOKEN)
 
